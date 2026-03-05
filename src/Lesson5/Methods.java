f9eca2122b6d1be2caf9e37ca845d592d3a22e6d
janedubrov <stupakova2012@gmail.com>
2026-03-05 11:51:41 +0100

    Loops

--- src/Lesson5/Methods.java
+++ src/Lesson5/Methods.java
@@ -1,14 +1,32 @@
 package Lesson5;
+import java.util.Scanner;
 
 public class Methods {
     public static void main(String[] args) {
-    int hours = 8;
-    int rateInEuro = 10;
-    int salary = calculateSalary(hours, rateInEuro);
-        System.out.println("Your daily salary is " + salary + " dollars") ;
-}
-static int calculateSalary(int hours, int rate){
-        return hours * rate;
+
+        Scanner input = new Scanner(System.in);
+
+        double temp;
+        double tempFahr;
+
+
+        System.out.print("What is the temperature in Celcius? : ");
+        temp = input.nextDouble();
+
+        tempFahr = convertedTemp(temp);
+        System.out.printf("Temperature in fahr is: %.2f C \n", tempFahr);
+
+        if(tempFahr > 90.0){
+            System.out.println("It is too hot outside! ");
+        }
+
+        input.close();
+
 
 }
+        static double convertedTemp(double temp){
+        return temp * 1.8 + 32;
+
+        }
+
 }
