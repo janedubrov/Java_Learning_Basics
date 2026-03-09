6118a29c5b26916149cbb8b43a6356f7b5bc5327
janedubrov <stupakova2012@gmail.com>
2026-03-09 15:34:17 +0100

    Arrays - practice

---
+++ src/Arrays/four.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class four {
+    public static void main(String[] args) {
+
+        String [] message = {"Kiwi" , "Mango"};
+        String [] message2 = {"Banana" , "Pineapple"};
+        String [] result = new String [message.length * 2];
+
+        for (int i = 0; i < message.length; i++){
+            result[i] = message[i];
+
+
+            }
+        for(int i = 0; i < message2.length; i++){
+            result[i + message.length] = message2[i];
+
+        }
+        System.out.println(Arrays.toString(result));
+
+
+
+
+    }
+
+}
+
+
